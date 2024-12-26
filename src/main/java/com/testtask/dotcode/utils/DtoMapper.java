bfb609a8bf93e4000501ec81d44eab2005b4351d
Ivan <vanyademglo@gmail.com>
2024-12-27 01:45:00 +0200

    added the util method for dto list mapping

--- src/main/java/com/testtask/dotcode/utils/DtoMapper.java
+++ src/main/java/com/testtask/dotcode/utils/DtoMapper.java
@@ -3,11 +3,13 @@ package com.testtask.dotcode.utils;
 import com.testtask.dotcode.dto.UserDto;
 import com.testtask.dotcode.domain.entity.User;
 
+import java.util.List;
+
 public class DtoMapper {
 
     public static User mapToUser(UserDto dto){
         User user = new User();
-
+        user.setId(dto.getId());
         user.setFirstName(dto.getFirstName());
         user.setLastName(dto.getLastName());
         user.setEmail(dto.getEmail());
@@ -16,10 +18,17 @@ public class DtoMapper {
 
     public static UserDto mapToUserDto(User user){
         UserDto dto = new UserDto();
-
+        dto.setId(user.getId());
         dto.setFirstName(user.getFirstName());
         dto.setLastName(user.getLastName());
         dto.setEmail(user.getEmail());
         return dto;
+    }
+
+    public static List<UserDto> toUserDtoList(List<User> users){
+        return users
+                .stream()
+                .map(DtoMapper::mapToUserDto)
+                .toList();
     }
 }
