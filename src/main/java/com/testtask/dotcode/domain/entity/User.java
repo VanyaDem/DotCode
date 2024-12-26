9eea24759205c045454fbe6e3673e1d75455c81d
Ivan <vanyademglo@gmail.com>
2024-12-26 20:29:36 +0200

    added User entity

---
+++ src/main/java/com/testtask/dotcode/domain/entity/User.java
@@ -0,0 +1,23 @@
+package com.testtask.dotcode.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+}
