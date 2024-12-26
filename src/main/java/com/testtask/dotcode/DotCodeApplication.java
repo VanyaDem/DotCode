31b757de39659abe29664c71da10d08ba7ba84e1
Ivan <vanyademglo@gmail.com>
2024-12-26 20:28:34 +0200

    added database connection property

---
+++ src/main/java/com/testtask/dotcode/DotCodeApplication.java
@@ -0,0 +1,13 @@
+package com.testtask.dotcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DotCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DotCodeApplication.class, args);
+    }
+
+}
