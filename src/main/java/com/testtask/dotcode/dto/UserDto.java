bfe770ff09eadf8ae86ea50a5c5146c77865b61c
Ivan <vanyademglo@gmail.com>
2024-12-29 00:08:44 +0200

    added size validation

--- src/main/java/com/testtask/dotcode/dto/UserDto.java
+++ src/main/java/com/testtask/dotcode/dto/UserDto.java
@@ -2,6 +2,7 @@ package com.testtask.dotcode.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -9,12 +10,15 @@ public class UserDto {
 
     private Long id;
 
+    @Size(max = 255)
     @NotBlank(message = "The first name must not be empty.")
     private String firstName;
 
+    @Size(max = 255)
     @NotBlank(message = "The last name must not be empty.")
     private String lastName;
 
+    @Size(max = 255)
     @Email(message = "Email should be valid")
     @NotBlank
     private String email;
