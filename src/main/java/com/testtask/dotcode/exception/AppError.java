7d16112c42b128791fc7d0d73057d49c08f52f3e
Ivan <vanyademglo@gmail.com>
2024-12-30 15:24:23 +0200

    refactored App Error to the Spring standards error view

--- src/main/java/com/testtask/dotcode/exception/AppError.java
+++ src/main/java/com/testtask/dotcode/exception/AppError.java
@@ -1,21 +1,35 @@
 package com.testtask.dotcode.exception;
 
 import lombok.Data;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Data
 public class AppError {
 
-    private int status;
+    private String timestamp;
+
+    private Integer status;
+
+    private String error;
 
     private String message;
+
+    private String path;
 
     private AppError() {
     }
 
-    public static AppError of(int status, String message) {
+    public static AppError of(HttpStatus status, String message, WebRequest request) {
         AppError error = new AppError();
-        error.setStatus(status);
-        error.setMessage(message);
+        error.timestamp = LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+        error.status = status.value();
+        error.error = status.getReasonPhrase();
+        error.message = message;
+        error.path = request.getDescription(false).replace("uri=", "");
         return error;
     }
 }
--- src/main/java/com/testtask/dotcode/exception/GlobalExceptionHandler.java
+++ src/main/java/com/testtask/dotcode/exception/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -13,7 +14,7 @@ import java.util.Map;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException exception) {
+    public ResponseEntity<AppError> handleValidationExceptions(MethodArgumentNotValidException exception, WebRequest request) {
 
         Map<String, String> errors = new HashMap<>();
         exception
@@ -21,23 +22,34 @@ public class GlobalExceptionHandler {
                 .getFieldErrors()
                 .forEach(error -> errors.put(error.getField(), error.getDefaultMessage())
                 );
-        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+
+        String message = errors
+                .entrySet()
+                .stream()
+                .map(entry -> entry.getKey() + ": " + entry.getValue())
+                .reduce((e1, e2) -> e1 + ", " + e2)
+                .orElse("");
+
+        var error = AppError.of(HttpStatus.BAD_REQUEST, message, request);
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<AppError> noSuchElementExceptionHandler(UserNotFoundException exception) {
-        var error = AppError.of(400, exception.getMessage());
+    public ResponseEntity<AppError> noSuchElementExceptionHandler(UserNotFoundException exception, WebRequest request) {
+        var error = AppError.of(HttpStatus.BAD_REQUEST, exception.getMessage(), request);
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
+
     @ExceptionHandler(UserEmailExistException.class)
-    public ResponseEntity<AppError> userEmailExistExceptionHandler(UserEmailExistException exception) {
-        var error = AppError.of(409, exception.getMessage());
+    public ResponseEntity<AppError> userEmailExistExceptionHandler(UserEmailExistException exception, WebRequest request) {
+        var error = AppError.of(HttpStatus.CONFLICT, exception.getMessage(), request);
         return new ResponseEntity<>(error, HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler
-    public ResponseEntity<AppError> exceptionHandler(RuntimeException exception) {
-        var error = AppError.of(500, exception.getMessage());
+    public ResponseEntity<AppError> exceptionHandler(RuntimeException exception, WebRequest request) {
+        var error = AppError.of(HttpStatus.INTERNAL_SERVER_ERROR, exception.getMessage(), request);
         return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
